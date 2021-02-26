67594f0ecf34236f02cd8960f8b0fe2a54381046
Ahmad AbdulRahman <52632898+AHR9N@users.noreply.github.com>
2021-02-26 16:25:04 +0200

    Add files via upload

---
+++ 2021/A.cpp
@@ -0,0 +1,25 @@
+#include "bits/stdc++.h"
+#pragma GCC optimize("Ofast")
+
+using namespace std;
+
+const int N = 1E+5, M = 1E6+5, MOD = 1E9+7;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+//    freopen("out.txt","w",stdout);
+    cout << "3\n"
+            "1\n"
+            "2\n"
+            "rue-d-athenes 1\n"
+            "rue-d-amsterdam 2\n"
+            "0\n"
+            "1\n"
+            "rue-de-londres 2\n"
+            "2\n"
+            "1\n"
+            "rue-de-moscou 1";
+
+    return 0;
+}
